b993d19fcc5571f5ad8a1fd1adfa97ecc4dd0c63
Rhony Septian <rseptian08@gmail.com>
2019-08-30 15:03:10 +0700

    Added auth handler

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('campaign', 'CampaignController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
